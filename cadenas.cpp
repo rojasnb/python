4c7b193c63e725b5f307ac9005a086b3078165eb
Bastian Rojas <bastian.rojasn@sansano.usm.cl>
2022-08-31 13:21:18 -0400

    Clase 31-8-2022, tipos de variables.

---
+++ cadenas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string str = "This is string object in C++";
+    cout << "String entered is: " << str;
+    return 0;
+}
